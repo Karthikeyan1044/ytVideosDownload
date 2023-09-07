7d6d12b21af39ab68d80f95763b3bacada580771
Karthikeyan Duraipandi <karthikeyan.d@finstein.ai>
2023-09-07 11:02:04 +0530

    Add project files.

---
+++ Controllers/YtController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using YoutubeExplode;
+//using YoutubeExplode.Models.MediaStreams;
+
+namespace ytVideosDownload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YtController : ControllerBase
+    {
+        [HttpPost("download")]
+        public async Task<IActionResult> DownloadVideoAsync([FromBody] string youtubeLink)
+        {
+            try
+            {
+                var youtube = new YoutubeClient();
+                string videoId = youtubeLink;
+                string fileName = GenerateFileName();
+                if (youtubeLink.Contains("/"))
+                {
+                    string[] parts = youtubeLink.Split('/');
+                    videoId = parts[parts.Length - 1];
+                }
+
+
+                if (youtubeLink != null)
+                {
+                    var streamInfoSet = await youtube.Videos.Streams.GetManifestAsync(youtubeLink);
+                    var streamInfo = streamInfoSet.GetMuxedStreams();
+
+                    // Sort by video quality and select the highest quality stream
+                    var videoStream = streamInfo
+                        .OrderByDescending(s => s.VideoQuality)
+                        .FirstOrDefault();
+
+                    if (videoStream != null)
+                    {
+                        using (var httpClient = new HttpClient())
+                        {
+                            // Set a timeout for the HTTP request (e.g., 5 minutes)
+                            httpClient.Timeout = TimeSpan.FromMinutes(2);
+
+                            // Download the video
+                            var videoBytes = await httpClient.GetByteArrayAsync(videoStream.Url);
+
+                            // Save the video to a file
+                            var filePath = Path.Combine("C:\\youtube", fileName);
+                            await System.IO.File.WriteAllBytesAsync(filePath, videoBytes);
+
+                            return Ok($"Video downloaded and saved as {filePath}");
+                        }
+                    }
+                }
+
+                return NotFound("Video not found");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+
+        private string GenerateFileName()
+        {
+            // Generate a unique file name for the downloaded video
+            string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".mp4";
+            return fileName;
+        }
+    }
+}
