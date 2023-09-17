a62b7027b8a3908cb236ddfbea3a4002ef6f9ace
Karthikeyan Duraipandi <karthikeyan.d@finstein.ai>
2023-09-17 14:00:56 +0530

    addd to file name

--- Controllers/YtController.cs
+++ Controllers/YtController.cs
@@ -18,13 +18,13 @@ namespace ytVideosDownload.Controllers
     public class YtController : ControllerBase
     {
         [HttpPost("download")]
-        public async Task<IActionResult> DownloadVideoAsync([FromBody] string youtubeLink)
+        public async Task<IActionResult> DownloadVideoAsync(string youtubeLink)
         {
             try
             {
                 var youtube = new YoutubeClient();
-                string videoId = youtubeLink;
                 string fileName = GenerateFileName();
+                string videoId = youtubeLink;
                 if (youtubeLink.Contains("/"))
                 {
                     string[] parts = youtubeLink.Split('/');
@@ -47,7 +47,7 @@ namespace ytVideosDownload.Controllers
                         using (var httpClient = new HttpClient())
                         {
                             // Set a timeout for the HTTP request (e.g., 5 minutes)
-                            httpClient.Timeout = TimeSpan.FromMinutes(2);
+                          //  httpClient.Timeout = TimeSpan.FromMinutes(2);
 
                             // Download the video
                             var videoBytes = await httpClient.GetByteArrayAsync(videoStream.Url);
